bd1b73767ccaac28998bb62cbf4b117857645d03
linxingsong <linxingsong@gmail.com>
2017-09-04 12:28:56 -0400

    done

--- HW1/HW1/main.cpp
+++ HW1/HW1/main.cpp
@@ -66,65 +66,50 @@ string check_result(double a, double b){
         return "They are not equal, A is "+to_string(a)+" and B is "+to_string(b);
     }
 }
+
 //1.4 Yield from bond price
-int yield_from_price(double F, double c, int n, double B_market, double tol, int max_iter, double & y, double & B)
+int yield_from_price(double F, double c, int n, double B_market, double tol, int max_iter, double & y)
 {
-    //step 1: a)the bond price decreases as the yield increases.
-    //        b)the bond price is a continuous function of the yield.
-    //step 2:
-    double y_low=0;
-    double y_high=100;
-    //step 3: Set ylow = 0 and calculate an output B_y_low using price from yield(F, c, y low, n,B y low).
-    double B_y_low;
-    price_from_yield(F, c, y_low, n, B_y_low);
-    //step 4:
-    if(abs(B_y_low-B_market) <= tol){
-        y=y_low;
-        return 0; //success
-    }
-    //step 5:
-    if(B_y_low < B_market){
-        y=0;
-        return 1; //fail
-    }
-    //step 6:
-    double B_y_high;
-    price_from_yield(F, c, y_low, n, B_y_high);
-    //step 7:
-    if(abs(B_y_high-B_market) <= tol){
-        y=y_high;
-        return 0; //success
-    }
-    //step 8:
-    if(B_y_high > B_market){
-        y=0;
-        return 1; //fail
-    }
-    //step 9: B_y_low > B_market >B_y_high
-    //step 10 &11: loop
+    double B, B_y_low, B_y_high, y_low, y_high;
+    y_low=0; y_high=100;
+    
     for(int i=0; i<max_iter; ++i){
-        //step 12:
         y=(y_low+y_high)/2.0;
+        price_from_yield(F,c, y_low, n, B_y_low);
+        price_from_yield(F, c, y_high, n, B_y_high);
         price_from_yield(F, c, y, n, B);
-        //step 13, 14,15:
+        
+        if((y_high-y_low)<=tol){
+            return 0;
+        }
+        
         if(abs(B-B_market)<=tol){
             return 0;
-        }else if(B>B_market && B_market<=tol){
+        }else if (B>B_market && B_market<= tol){
             y_low=y;
-        }else if(B<B_market && B_market<=tol){
+        }else{
             y_high=y;
         }
-        //step 16
-        if(y_high-y_low<=tol){
+        
+        if(abs(B_y_low-B_market) <= tol){
+            y=y_low;
             return 0;
         }
-        //step 17, 18
+        
+        if(abs(B_y_high-B_market)<=tol){
+            y=y_high;
+            return 0;
+        }
+        
+        if(B_y_low< B_market || B_y_high>B_market){
+            y=0;
+            return 1;
+        }
         if(i==max_iter){
             y=0;
             return 1;
         }
     }
-    //step 19: still not return 1, so it will be 0, success.
     return 0;
 }
 
@@ -139,7 +124,7 @@ int main(int argc, const char * argv[]) {
     
     //call function, return value.
     F1=future_value(F0, t0, t1, r);
-    cout<<"For 1.1: Future value, the F0, t0, t1. and r is given in code, reult F1 is "+to_string(F1)<<endl;
+    cout<<"For 1.1: Future value, the F0, t0, t1. and r is given in code, result F1 is "+to_string(F1)<<endl;
     cout<<"=====================================\n\n"<<endl;
     
     //calculated discount factor: do it in method.
@@ -156,55 +141,61 @@ int main(int argc, const char * argv[]) {
     //set y=0 check it
     y=0; B=0;
     price_from_yield(F, c, y, n, B);
-    cout<<"Case 2: checking if y=0"<<endl;
+    cout<<"Case 2: checking if y=0, result: "<<endl;
     temp=F+(n*c)/2;
     cout<<check_result(B, temp)<<endl;
     //set y=12 back, c=0
     y=10; c=0; B=0;
     price_from_yield(F, c, y, n, B);
-    cout<<"Case 3: checking if c=0"<<endl;
+    cout<<"Case 3: checking if c=0, result: "<<endl;
     temp = F / pow((1+(0.5*y*0.01)), n);
     cout<<check_result(B, temp)<<endl;
     
     cout<<"======================================\n\n"<<endl;
     cout<<"1.4: Yield from bond price"<<endl;
     //set value of c back
-    c=10; B=0;
+    c=10; F=100; y=10; n=100; F0=123; t0=0; t1=15; r=5;
     //continues steps in function yield_from_price, for checking
     //step 20: test case
+    
     //step 21:
-    double B_market =100; double tol =335.0; int max_iter=50;
-    yield_from_price(F, c, n, B_market, tol, max_iter, y, B);
+    double B_market =100; double tol =25; int max_iter=100;
     cout<<"test case for step 21: "<<endl;
+    cout<<yield_from_price(F, c, n, B_market, tol, max_iter, y)<<endl;
     cout<<"y = "<<to_string(y)<<" c = "<<to_string(c)<<endl; //error
-    //reset B, y
+    
     B=0; y=10;
+    price_from_yield(F, c, y, n, B);
+    cout<<"check F=100, set y==c, B is "<<to_string(B)<<endl;
+    
     //step 22:
     B_market=50;
-    
-    yield_from_price(F, c, n, B_market, tol, max_iter, y, B);
+    y=10;
     cout<<"step 22:\ncase 1: B_market <100 "<<endl;
+    cout<<yield_from_price(F, c, n, B_market, tol, max_iter, y)<<endl;
     if(y>c){
         cout<<"Correct, y>c, where y="+ to_string(y)+" c="+to_string(c)<<endl;
     }else{
         cout<<"error, y<c where y="+to_string(y)+ " c="+to_string(c)<<endl;
     }
     //reset
-    B=0; y=10;
+    y=10;
     B_market=200;
-    yield_from_price(F, c, n, B_market, tol, max_iter, y, B);
-    cout<<"case 2: B_market<100"<<endl;
+    cout<<"case 2: B_market>100"<<endl;
+    cout<<yield_from_price(F, c, n, B_market, tol, max_iter, y)<<endl;
     if(y<c){
         cout<<"correct, y<c, where y="+ to_string(y)+" c="+to_string(c)<<endl;
     }else{
         cout<<"error, y>c where y="+to_string(y)+ " c="+to_string(c)<<endl;
     }
     //step 23:
-    B=0; y=10; B_market=150;
+    y=10;
+    B_market=150;
     c=0;
-    yield_from_price(F, c, n, B_market, tol, max_iter, y, B);
     temp = F / pow((1+(0.5*y*0.01)), n);
-    cout<<"Step 23 test case:\nZero coupon bond case check: "<<endl;
+    cout<<"Step 23 test case:"<<endl;
+    cout<<yield_from_price(F, c, n, B_market, tol, max_iter, y)<<endl;
+    cout<<"Zero coupon bond case check: "<<endl;
     cout<<"y is "+to_string(y)<<endl;
     return 0;
 }
