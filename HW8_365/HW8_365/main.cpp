2bab211266ac560e5bacb49538b17d63586b935f
XingSong Lin <linxingsong@gmail.com>
2017-11-27 12:18:54 -0500

    hw8

---
+++ HW8_365/HW8_365/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW8_365
+//
+//  Created by XingSong  Lin on 11/27/17.
+//  Copyright © 2017 XingSong  Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
