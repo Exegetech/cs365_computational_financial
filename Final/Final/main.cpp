3f43e460fcdbf47214601dd87c76a732856769a8
XingSong Lin <linxingsong@gmail.com>
2017-12-09 11:22:34 -0500

    working on

--- Final/Final/main.cpp
+++ Final/Final/main.cpp
@@ -75,7 +75,8 @@ int yield_from_price(double F, double c[], int n, double B_market, double tol, i
 
 void compute_Question1(double &y){
     ofstream outfile;
-    outfile.open("Solution_for_Queston_1.txt");
+    outfile.open("Solution_for_Q1.txt");
+    outfile<<"Student ID: 23083576 --> array of{2,3,0,8,3,5,7,6}\n\n";
     double c[8] = {2,3,0,8,3,5,7,6};
     double F = 100;
     y = 5;
@@ -87,8 +88,8 @@ void compute_Question1(double &y){
     outfile<<"The theoretical fair value of the bond is "<<fixed<<setprecision(2)<<B<<" if yield of the bond is 5%.\n\n";
     double B_market = 100;
     yield_from_price(F, c, n, B_market, tol, max_iter, y);
-    outfile<<"The yield of the bond is "<<fixed<<setprecision(2)<<y<<"% if market price of the bond is 100.\n";
-    
+    outfile<<"The yield of the bond is "<<fixed<<setprecision(2)<<y<<"% if market price of the bond is 100.\n\n";
+    outfile<<"\n---------------------------Separated Line-----------------------------------\n\n";
     outfile.close();
 }
 
@@ -251,7 +252,7 @@ int BinomialModel::FairValue(int n, Derivative * p_derivative, double S, double 
     V = V_tmp[0];
     
     // deallocate memory (if necessary)
-    //Clear();
+    
     return 0;
 }
 
@@ -268,7 +269,7 @@ int BinomialModel::ImpliedVolatility(int n, Derivative * p_derivative, double S,
 int BinomialModel::ImpliedVolatilityPrivate(int n, Derivative * p_derivative, double S, double t0, double target,
                                             double & implied_vol, int & num_iter)
 {
-    const double tol = 1.0e-4;
+    const double tol = 1.0e-6;
     const int max_iter = 100;
     
     implied_vol = 0;
@@ -378,8 +379,46 @@ int Option::ValuationTests(double S, double &V) const
 //Question 5
 class straddle : public Derivative
 {
+public:
+    straddle(){K = 0; isCall = false; isAmerican = false;}
+    virtual ~straddle(){}
+    virtual double TerminalPayoff(double S) const;
+    virtual int ValuationTest(double S, double &V) const;
     
+    double K;
+    bool isCall;
+    bool isAmerican;
 };
+
+double straddle::TerminalPayoff(double S) const
+{
+    double temp =0.0;
+    if(isCall){
+        if(S>K){
+            temp+=S-K;
+        }
+    }else{
+        if(S<K){
+            temp+=K-S;
+        }
+    }
+    return temp;
+}
+
+int straddle::ValuationTest(double S, double &V) const
+{
+    if(isAmerican){
+        double temp =0.0;
+        if(isCall){
+            temp += max(V, max(S-K, 0.0));
+        }else{
+            temp += max(V, max(K-S, 0.0));
+        }
+        return temp;
+    }
+    
+    return 0;
+}
 
 //Question 6
 class BinaryOption : public Derivative
@@ -418,11 +457,200 @@ int BinaryOption::ValuationTests(double S, double &V) const
     return 0;
 }
 
+class UpOutBarrierCallOption: public Derivative
+{
+public:
+    UpOutBarrierCallOption(){K=0; B=0; isCall=false; isAmerican=false;}
+    virtual ~UpOutBarrierCallOption(){}
+    virtual double TerminalPayoff(double S) const;
+    virtual int ValuationTests(double S, double &V) const;
+    // data
+    double K;
+    double B; //barrier
+    bool isCall;
+    bool isAmerican;
+};
+
+double UpOutBarrierCallOption::TerminalPayoff(double S) const
+{
+    //K<B , and S depends.
+    if(K<=S && S<B){
+        return S-K;
+    }
+    if(S>=B){
+        return B-K;
+    }
+    if(S<K){
+        return 0;
+    }
+    return 0;
+}
+
+int UpOutBarrierCallOption::ValuationTests(double S, double &V) const
+{
+    if(isAmerican){
+        double V_barrier = B-K;
+        if(K<S && S<B && V_barrier<(S-K)){
+            return V_barrier;
+        }
+    }
+    return 0;
+}
+
+int Question4_test(){
+    int rc = 0;
+    
+    // output file
+    ofstream ofs;
+    ofs.open("Solution_for_Q4.txt");
+    
+    //srand((unsigned)time(NULL));
+    
+    double S = 590;
+    double K = 150;
+    double r = 0.12;
+    double q = 0.04;
+    double sigma = 0.8;
+    double T = 2.0;
+    double t0 = 1.3;
+    /*
+    double S = rand();
+    double K = rand();
+    double r = (double)(rand()%101)/101;
+    double q = (double)(rand()%101)/101;
+    double sigma = (double)(rand()%101)/101;
+    double T = (double)(rand()%101)/101;
+    double t0 = (double)(rand()%101)/101;
+    
+    ofs<<S<<" "<<K<<" "<<r<<" "<<q<<" "<<sigma<<" "<<T<<" "<<t0<<endl;
+    */
+    
+    Option Eur_put;
+    Eur_put.r = r;
+    Eur_put.q = q;
+    Eur_put.sigma = sigma;
+    Eur_put.T = T;
+    Eur_put.K = K;
+    Eur_put.isCall = false;
+    Eur_put.isAmerican = false;
+    
+    Option Am_put;
+    Am_put.r = r;
+    Am_put.q = q;
+    Am_put.sigma = sigma;
+    Am_put.T = T;
+    Am_put.K = K;
+    Am_put.isCall = false;
+    Am_put.isAmerican = true;
+    
+    Option Eur_call;
+    Eur_call.r = r;
+    Eur_call.q = q;
+    Eur_call.sigma = sigma;
+    Eur_call.T = T;
+    Eur_call.K = K;
+    Eur_call.isCall = true;
+    Eur_call.isAmerican = false;
+    
+    Option Am_call;
+    Am_call.r = r;
+    Am_call.q = q;
+    Am_call.sigma = sigma;
+    Am_call.T = T;
+    Am_call.K = K;
+    Am_call.isCall = true;
+    Am_call.isAmerican = true;
+    
+    double FV_Am_put = 0;
+    double FV_Eur_put = 0;
+    double FV_Am_call = 0;
+    double FV_Eur_call = 0;
+    
+    int n = 100;
+    
+    BinomialModel binom(n);
+    
+    double dS = 0.1;
+    int imax = 2000;
+    for (int i = 1; i <= imax; ++i) {
+        S = i*dS;
+        rc = binom.FairValue(n, &Am_put, S, t0, FV_Am_put);
+        rc = binom.FairValue(n, &Eur_put, S, t0, FV_Eur_put);
+        rc = binom.FairValue(n, &Am_call, S, t0, FV_Am_call);
+        rc = binom.FairValue(n, &Eur_call, S, t0, FV_Eur_call);
+    }
+    ofs << setw(16) << "S" << " ";
+    ofs << setw(16) << "FV_Am_put" << " ";
+    ofs << setw(16) << "FV_Eur_put" << " ";
+    ofs << setw(16) << "FV_Am_call" << " ";
+    ofs << setw(16) << "FV_Eur_call" << " ";
+    ofs << endl;
+    ofs << setw(16) << S << " ";
+    ofs << setw(16) << FV_Am_put << " ";
+    ofs << setw(16) << FV_Eur_put << " ";
+    ofs << setw(16) << FV_Am_call << " ";
+    ofs << setw(16) << FV_Eur_call << " ";
+    ofs << endl;
+    ofs <<"---------------------------Separated Line-----------------------------------\n\n";
+    
+    //test 4.1
+    ofs << "Test for Equation 4.1: \n\n";
+    double tol = 1.0e-6;
+    double V_c_sub_p = FV_Eur_call - FV_Eur_put;
+    double V_C_sub_P = FV_Am_call - FV_Am_put;
+    double dt = T-t0;
+    double e1 = exp(-q*dt);
+    double V_Se = S * e1;
+    double e2 = exp(-r*dt);
+    double V_Ke = K * e2;
+    bool check_result=false;
+    /*
+    ofs<<"Computed: \n";
+    ofs<<"tol: "<<tol;
+    ofs<<"\nc-p: "<<V_c_sub_p;
+    ofs<<"\nC-P: "<<V_C_sub_P;
+    ofs<<"\nS*e~: "<<V_Se;
+    ofs<<"\nK*e~: "<<e2<<endl;
+    */
+    ofs<<"Test--> 4.1: \n\n";
+    if(abs((V_c_sub_p)-(V_Se-V_Ke)) <= tol){
+        check_result = true;
+    }else{
+        check_result = false;
+    }
+    ofs<<"Result for passing equation 4.1 is "<<boolalpha<<check_result<<".\n\n";
+    
+    ofs<<"Test--> 4.2: \n\n";
+    if((V_C_sub_P >= (V_Se-K-tol)) && (V_C_sub_P<=(S-V_Ke+tol))){
+        check_result = true;
+    }else{
+        check_result = false;
+    }
+    ofs<<"Result for passing equation 4.2 is "<<check_result<<".\n\n";
+    
+    ofs<<"Test--> 4.3: \n\n";
+    if((0<= FV_Eur_call && FV_Eur_call <= (V_Se+tol)) && (0<=FV_Am_call && FV_Am_call <= S+tol) &&
+       (0<= FV_Eur_put && FV_Eur_put <= (V_Se+tol)) && (0<=FV_Am_put && FV_Am_put<= K+tol)){
+        check_result = true;
+    }
+    else{
+        check_result = false;
+    }
+    ofs<<"Result for passing equation 4.3 is "<<check_result<<".\n\n";
+    
+    ofs.close();
+    return 0;
+}
 
 int main(int argc, const char * argv[]) {
+    
     double yield;
     compute_Question1(yield);
+    ofstream of;
+    of.open("Solution_for_Q1.txt", ios_base::app);
+    of<<"yield without rounding to 2 decimal is "<<yield<<"\n";
+    of.close();
     
-    
+    Question4_test();
     return 0;
 }
